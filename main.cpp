df881f5919d9570594c53610194f58edfbd81781
@a01273096 <darc3012@gmail.com>
2020-05-21 12:19:06 -0500

    feat: Programación dinámica

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int shortestDist();
+
+int main() {
+	shortestDist();
+	return 0;
+}
+
+
+
---
+++ progDinamica.cpp
@@ -0,0 +1,36 @@
+/*
+	Distancia más corta usando programación dinámica
+*/
+
+#include <iostream>
+using namespace std;
+
+#include "matrix.h"
+#define T 12
+
+int shortestDist() {
+	short** A = createMatrix<short>(T);
+	initialize(A, T);
+	cargarEjemplo<short>(A);
+	printMatrix(A, T);
+
+	int distancia[T];
+	distancia[T - 1] = 0;
+
+	for (int i = T - 2; i >= 0; i--)
+	{
+		distancia[i] = INF;
+
+		for (int j = i; j < T; j++)
+		{
+			if (*(*(A + i) + j) == INF)
+				continue;
+
+			distancia[i] = (distancia[i] < *(*(A + i) + j) + distancia[j]) ? distancia[i] : *(*(A + i) + j) + distancia[j];
+		}
+	}
+
+	cout << endl << "La distancia minima es: " << distancia[0] << endl;
+	deleteMatrix(A, T);
+	return distancia[0];
+}
