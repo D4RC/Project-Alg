69c1cc004114102592ca074e53c48e99c97326fc
DARC <darc3012@gmail.com>
2020-05-30 19:35:41 -0500

    refac: Example separation

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -4,10 +4,12 @@
 #include "matrix.h"
 #define T 12
 
+void loadExample1(short**);
+
 int shortestDist() {
 	short** A = createMatrix<short>(T);
 	initialize(A, T);
-	loadExample1<short>(A);
+	loadExample1(A);
 	printMatrix(A, T);
 
 	int distance[T];
@@ -29,4 +31,30 @@ int shortestDist() {
 	cout << endl << "La distancia minima es: " << distance[0] << endl;
 	deleteMatrix(A, T);
 	return distance[0];
+}
+
+void loadExample1(short** mat) {
+	mat[0][1] = 6;
+	mat[0][2] = 8;
+	mat[0][3] = 7;
+	mat[1][4] = 9;
+	mat[1][5] = 10;
+	mat[2][4] = 5;
+	mat[2][5] = 7;
+	mat[2][6] = 5;
+	mat[2][7] = 6;
+	mat[3][6] = 8;
+	mat[3][7] = 14;
+	mat[4][8] = 10;
+	mat[4][9] = 8;
+	mat[5][8] = 7;
+	mat[5][9] = 9;
+	mat[6][8] = 11;
+	mat[6][9] = 8;
+	mat[6][10] = 7;
+	mat[7][9] = 12;
+	mat[7][10] = 6;
+	mat[8][11] = 14;
+	mat[9][11] = 6;
+	mat[10][11] = 15;
 }
--- matrix.h
+++ matrix.h
@@ -71,30 +71,3 @@ void initialize(T** mat, size_t n) {
 		for (size_t j = 0; j < n; j++)
 			* (*(mat + i) + j) = INF;
 }
-
-template <class T>
-void loadExample1(T **mat) {
-	mat[0][1]=6;
-	mat[0][2]=8;
-	mat[0][3]=7;
-	mat[1][4]=9;
-	mat[1][5]=10;
-	mat[2][4]=5;
-	mat[2][5]=7;
-	mat[2][6]=5;
-	mat[2][7]=6;
-	mat[3][6]=8;
-	mat[3][7]=14;
-	mat[4][8]=10;
-	mat[4][9]=8;
-	mat[5][8]=7;
-	mat[5][9]=9;
-	mat[6][8]=11;
-	mat[6][9]=8;
-	mat[6][10]=7;
-	mat[7][9]=12;
-	mat[7][10]=6;
-	mat[8][11]=14;
-	mat[9][11]=6;
-	mat[10][11]=15;
-}
